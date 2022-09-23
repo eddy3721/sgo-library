f1f103cdd4b42f8cb7826d4e70eb51b60bf6c6a3
eddy3721 <102030818+eddy3721@users.noreply.github.com>
2022-09-23 18:23:33 +0800

    Add files via upload

--- src/Drop.js
+++ src/Drop.js
@@ -1,9 +1,59 @@
 import React from 'react'
+import PropData from './json/props.json'
+import styles from './css/drop.module.css';
+import { useState } from 'react';
+import Title from './Title';
 
 export default function Drop() {
+  let type = [["item", "道具"],["mine", "礦物"]];
+  let propClass = {};
+
+  type.forEach(type=>{
+    propClass[type[0]] = PropData.filter(prop=>prop.type === type[0]);
+  });
+
+  let [typeNum, setTypeNum] = useState('');
+  let [openIntro, setOpenIntro] = useState(0);
+
+  const showIntro = (id)=>{
+    (openIntro === id) ? setOpenIntro(0) : setOpenIntro(id);
+  }
+
   return (
-    <div>
-      <h1>素材</h1>
-    </div>
+      <div>
+        <Title mainTitle="素材"/>
+        
+        <div className='main'>
+          <div>
+            {
+              type.map(type=>(
+                <button onClick={()=>setTypeNum(type[0])} className={styles.enemyMapButton}>{type[1]}</button>
+              ))
+            }
+          </div>
+          <hr className="divider" />
+          <div>
+            {
+              (typeNum) &&
+              propClass[typeNum].map(item=>(
+                <div key={item.id} className={styles.item} onClick={showIntro.bind(this,item.id)}>
+                  {
+                    (openIntro === item.id) ?
+                    <span>&nbsp;&nbsp;&nbsp;◆&nbsp;&nbsp;&nbsp;</span>:
+                    <span>&nbsp;&nbsp;&nbsp;◇&nbsp;&nbsp;&nbsp;</span>
+                  }
+                  <span>{item.name}</span>
+                  <div>
+                    {
+                      (openIntro === item.id) &&
+                      <div>{item.intro}</div>
+                    }
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
   )
 }
--- src/Skill.js
+++ src/Skill.js
@@ -1,9 +1,60 @@
 import React from 'react'
+import PropData from './json/props.json'
+import styles from './css/enemy.module.css';
+import { useState } from 'react';
+import Title from './Title';
 
 export default function Skill() {
+  let type = ["單手劍", "細劍", "短刀", "單手錘", "盾牌", "雙手劍", "太刀", "雙手斧", "長槍"];
+  let [weaponType, setWeaponType] = useState('');
+
+  let skillData = PropData.filter(prop=>prop.type === 'skill');
+  let weaponClass = {};
+  for(let i = 0; i < type.length; i++){
+    weaponClass[type[i]] = skillData.filter(prop=>prop.weapon === type[i]);
+  }
+
   return (
     <div>
-      <h1>技能</h1>
-    </div>
+        <Title mainTitle="技能"/>
+        
+        <div className='main'>
+          <div>
+            {
+              type.map(weapon=>(
+                <button onClick={()=>setWeaponType(weapon)} className={styles.enemyMapButton}>{weapon}</button>
+              ))
+            }
+          </div>
+          <hr className="divider" />
+          <div>
+            {
+              (weaponType === '') ? 
+              <div>-請選擇武器類型-</div>:
+              <div>{'-' + weaponType + '-'}</div>
+            }
+          </div>
+          
+          <div className={styles.enemyList}>
+            {
+              weaponType &&
+              weaponClass[weaponType].map((skill)=>(
+                <div className={styles.skillDetail}>
+                  <p>{skill.name}</p>
+                  <p>{skill.intro}</p>
+                  <hr className="divider" />
+                  {
+                    skill.preview.map((p, index)=>(
+                      <div className={styles.report}>
+                        <div className={styles.reportNum}>{index+1}</div>{p}
+                      </div>
+                    ))
+                  }
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
   )
 }
