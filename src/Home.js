23df1c59723be54ee25345812dc507a50232b564
eddy3721 <102030818+eddy3721@users.noreply.github.com>
2022-09-25 21:21:54 +0800

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -9,17 +9,26 @@ export default function ProductList() {
         {'name':'素材','url':'/drop'},
         {'name':'技能','url':'/skill'},
         {'name':'未開放','url':'/none'},
+        {'name':'未開放','url':'/none'},
+        {'name':'未開放','url':'/none'},
+        {'name':'未開放','url':'/none'},
+        {'name':'未開放','url':'/none'},
     ];
 
     return (
         <div >
             <h1>大木小窩</h1>
-            <nav className={styles.homeNav}>
-                {
-                    list.map(product=>(
-                        <Link to={product.url}>{product.name}</Link>
-                    ))
-                }
+
+            <nav className={styles.circleMenu}>
+                <div>
+                    {
+                        list.map(product=>(
+                            <Link to={product.url} className={styles.circleButton}>
+                                <Link to={product.url}>{product.name}</Link>
+                            </Link>
+                        ))
+                    }
+                </div>
             </nav>
         </div>
     )
